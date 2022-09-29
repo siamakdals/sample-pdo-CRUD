51653734b026a32196d5b092bbb2c6eb61b043e5
siamak <siamakdals@gmail.com>
2022-09-29 14:52:28 +0330

    Big update: use try catch for all methods prepare statements log all errors with more data of them new methods and ...

--- DB.php
+++ DB.php
@@ -1,11 +1,11 @@
 <?php
 
-
 class DB
 {
 
     private $conn = '';
     private $log_errors = false;
+    private $bound = [];
 
     public function __construct($db = "", $username = "", $password = "", $servername = "localhost", $log_errors = true)
     {
@@ -13,81 +13,83 @@ class DB
 
         try {
             $this->conn = new PDO("mysql:host=$servername;dbname=$db", $username, $password);
+            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
             $this->conn->exec("set names utf8mb4");
-        } catch
-        (PDOException $e) {
-            //echo "Connection failed: " . $e->getMessage();
+        } catch (PDOException $e) {
+            //Connection failed
             $this->logError($e->getMessage());
         }
 
     }
 
-    public function query($sql)
+    public function query($sql, $params = [])
     {
-        $stm = $this->conn->prepare($sql);
-        $res = $stm->execute();
-        $this->logError($sql);
-        return $res;
+        try {
+            $stm = $this->conn->prepare($sql);
+            $stm->execute($params);
+            return $stm->rowCount();
+        } catch (Exception $e) {
+            $this->logErrorWrapper($stm, $e);
+            return null;
+        }
     }
 
     public function insertData($table, $fields, $data)
     {
+        try {
 
-        if (is_array($fields))
-            $fields = implode(',', $fields);
+            $count = 1;
+            if (is_array($fields)) {
+                $count = count($fields);
+                $fields = implode(',', $fields);
+            }
 
-        if (is_array($data))
-            $data = implode("','", $data);
+            $question_mark = implode(',', array_fill(0, $count, '?'));
 
-        $data = "'" . $data . "'";
+            $sql = "INSERT INTO `$table` ($fields) VALUES ($question_mark)";
 
-        $sql = "INSERT INTO $table ($fields) VALUES ($data)";
-        $res = $this->conn->exec($sql);
-        $this->logError($sql);
+            $smt = $this->conn->prepare($sql);
 
-        if ($res)
+            $data = is_array($data) ? $data : [$data];
+            $smt->execute($data);
+            $smt->closeCursor();
+
             return $this->conn->lastInsertId();
-        else
-            return null;
+        } catch (Exception $e) {
+            $this->logErrorWrapper($smt, $e);
+            return 0;
+        }
     }
 
     public function multiInsert(array $data, array $table_columns, $table)
     {
         $fields = implode(',', $table_columns);
-        $sql = [];
 
-        foreach ((array)$data as $row) {
-            $sql[] = "('" . implode("','", $row) . "')";
+        $values = [];
+        $params = [];
+
+        foreach ($data as $row) {
+            $params = array_merge($params, array_values($row));
+            $values[] = "(" . implode(',', array_fill(0, count($row), '?')) . ")";
         }
 
-        $values = implode(',', $sql);
+        $values = implode(',', $values);
 
-        return $this->query("INSERT INTO {$table} ({$fields}) VALUES {$values}");
+        return $this->query("INSERT INTO {$table} ({$fields}) VALUES {$values}",$params);
     }
 
     public function getData($table, $fields = '*', array $where_key_data, $order = 'ASC', $order_by = 'id',
                             $limit = 0, $fetchAll = false, $offset = 0, $search = null, $group_by = null)
     {
-        $where = '';
-        foreach ($where_key_data as $key => $data) {
-            $last_key = $key;
-        }
-        foreach ($where_key_data as $key => $data) {
-            $virgol = $key == $last_key ? '' : ' AND ';
-            $where .= $key . "='" . $data . "'$virgol";
-        }
 
-        if (is_array($fields)) {
-            $fields = implode(',', $fields);
-        }
+        $where = $search ? $this->likeStatement($where_key_data) : $this->whereStatement($where_key_data);
+
+        $fields = is_array($fields) ? implode(',', $fields) : $fields;
 
         $sql = "SELECT $fields FROM $table";
         if (!empty($where))
-            $sql = "SELECT $fields FROM $table WHERE $where";
-
-        if (!empty($search)) {
-            $sql = "SELECT $fields FROM $table WHERE " . "`persian_title` LIKE '%$search%' OR `title` LIKE '%$search%'";
-        }
+            $sql = "SELECT $fields FROM $table $where";
 
         if (!empty($group_by))
             $sql .= " GROUP BY {$group_by}";
@@ -95,143 +97,190 @@ class DB
             $sql .= " ORDER BY {$order_by} {$order}";
         if ($order_by == 'id' && $order != 'ASC')
             $sql .= " ORDER BY {$order_by} {$order}";
+
+        $limit = (int)$limit;
+        $offset = (int)$offset;
         if (!empty($limit))
             $sql .= " LIMIT {$limit}";
         if (!empty($offset))
             $sql .= " OFFSET {$offset}";
-        $sql_smt = $sql;
 
-        $sql = $this->conn->prepare($sql);
-        $sql->execute();
-        if ($fetchAll)
-            $res = $sql->fetchAll(PDO::FETCH_ASSOC);
-        else
-            $res = $sql->fetch(PDO::FETCH_ASSOC);
+        try {
+            $smt = $this->conn->prepare($sql);
+            $smt->execute($this->getBound());
 
-        $this->logError($sql_smt);
-        if ($res)
+            if ($fetchAll)
+                $res = $smt->fetchAll(PDO::FETCH_ASSOC);
+            else
+                $res = $smt->fetch(PDO::FETCH_ASSOC);
+
+            $smt->closeCursor();
+
             return $res;
-        else
+        } catch (Exception $e) {
+            $this->logErrorWrapper($smt, $e);
             return null;
+        }
     }
 
-    public function getRow($sql)
+    public function getRow($sql, $params = [])
     {
-        $stm = $this->conn->prepare($sql);
-        $stm->execute();
-        $res = $stm->fetch(PDO::FETCH_ASSOC);
-        $this->logError($sql);
-        return $res;
-    }
-
-    public function getResult($sql)
-    {
-        $stm = $this->conn->prepare($sql);
-        $stm->execute();
-        $res = $stm->fetchAll(PDO::FETCH_ASSOC);
-        $this->logError($sql);
-        return $res;
-    }
-
-    public function updateData($table, array $key_data)
-    {
-        $sql = '';
-        $last_key = '';
-        foreach ($key_data as $key => $data) {
-            $last_key = $key;
-        }
-        foreach ($key_data as $key => $data) {
-            $virgol = $key == $last_key ? '' : ',';
-            $sql .= $key . "='" . $data . "'$virgol";
-        }
-
-        $where = '';
-        foreach ($where_key_data as $key => $data) {
-            $last_key = $key;
-        }
-        foreach ($where_key_data as $key => $data) {
-            $virgol = $key == $last_key ? '' : ' AND ';
-            $where .= $key . "='" . $data . "'$virgol";
-        }
-
-        $sql = "UPDATE $table SET $sql WHERE $where";
-        $res = $this->conn->exec($sql);
-
-        $this->logError($sql);
-        if ($res)
-            return true;
-        else
+        try {
+            $stm = $this->conn->prepare($sql);
+            $stm->execute($params);
+            $res = $stm->fetch(PDO::FETCH_ASSOC);
+            $stm->closeCursor();
+            return $res;
+        } catch (Exception $e) {
+            $this->logErrorWrapper($stm, $e);
             return null;
+        }
+    }
+
+    public function getResult($sql, $params = [])
+    {
+        try {
+            $stm = $this->conn->prepare($sql);
+            $stm->execute($params);
+            return $stm->fetchAll(PDO::FETCH_ASSOC);
+        } catch (Exception $e) {
+            $this->logErrorWrapper($stm, $e);
+            return null;
+        }
+    }
+
+    public function updateData($table, array $key_data, array $where_key_data)
+    {
+        $data_bound = [];
+        array_walk($key_data, function (&$value, $key) use (&$data_bound) {
+            $cursor_key = ":data_$key";
+            $data_bound[$cursor_key] = $value;
+            $value = "`$key`=$cursor_key";
+        });
+
+        $sql = implode(',', $key_data);
+        $where = $this->whereStatement($where_key_data);
+
+        $sql = "UPDATE $table SET $sql";
+        $sql = !empty($where) ? $sql . " $where" : $sql;
+
+        $params = array_merge($data_bound, $this->getBound());
+        return $this->query($sql, $params);
     }
 
     public function upsert($table, array $key_data)
     {
-        $sql = '';
-        $keys = array_keys($key_data);
-        $last_key = end($keys);
-        foreach ($keys as $key) {
-            $virgol = $key === $last_key ? '' : ',';
-            $sql .= $key . "='" . $key_data[$key] . "'$virgol";
-        }
 
+        $fields = implode(',', array_keys($key_data));
+        $question_mark_insert = implode(',', array_fill(0, count($key_data), '?'));
 
-        $fields = implode(',', $keys);
-        $data = implode("','", $key_data);
+        $params = array_values($key_data);
+        $params = array_merge($params, $params);
 
-        $data = "'" . $data . "'";
+        //
+        array_walk($key_data, function (&$value, $key) {
+            $value = "`$key`=?";
+        });
+        $question_mark_update = implode(',', $key_data);
 
+        $sql = "INSERT INTO {$table} ($fields) VALUES ($question_mark_insert)
+            ON DUPLICATE KEY UPDATE $question_mark_update;";
 
-        $sql = "INSERT INTO {$table} ($fields) VALUES ($data)
-            ON DUPLICATE KEY UPDATE $sql;";
-
-        return $this->query($sql);
+        return $this->query($sql, $params);
     }
 
     public function deleteData($table, array $where_key_data)
     {
-        $where = '';
-        $last_key = end(array_keys($where_key_data));
 
-        foreach ($where_key_data as $key => $data) {
-            $virgol = $key == $last_key ? '' : ' AND ';
-            $where .= $key . "='" . $data . "'$virgol";
-        }
+        $where = $this->whereStatement($where_key_data);
 
         $sql = "DELETE FROM $table";
-        if (!empty($where))
-            $sql .= " WHERE $where";
+        $sql = !empty($where) ? $sql . " $where" : $sql;
 
-        $res = $this->conn->exec($sql);
-
-        $this->logError($sql);
-
-        if ($res)
-            return true;
-        else
-            return null;
+        return $this->query($sql, $this->getBound());
     }
 
-    private function logError($sql = null)
+    public function whereStatement(array $where_key_data, $operator = 'AND')
     {
-        if (!$this->log_errors)
-            return;
-        if (!empty($this->conn) && ($this->conn->errorCode() == 0000))
-            return;
+        $bound = [];
+        array_walk($where_key_data, function (&$value, $key) use (&$bound) {
+            $bound[":$key"] = $value;
+            $value = "`$key`=:$key";
+        });
 
-        $txt = "\n-------| " . date('Y-m-d H:i:s') . " |-------\n";
+        $operator = ' ' . $operator . ' ';
+        $where = implode($operator, $where_key_data);
 
-        if (!empty($this->conn)) {
-            $txt .= var_export($this->conn->errorInfo(), true) . "\n";
-            $txt .= "SQL:\n$sql\n";
-        } else {
-            $txt .= "$sql\n";
+        if (empty($where)) {
+            return '';
         }
+
+        $this->bound = $bound;
+
+        return 'WHERE ' . $where;
+    }
+
+    public function likeStatement(array $column_data, $operator = 'OR')
+    {
+        $bound = [];
+        array_walk($column_data, function (&$value, $key) use (&$bound) {
+            $bound[":$key"] = '%' . $value . '%';
+            $value = "`$key` LIKE :$key";
+        });
+
+        $operator = ' ' . $operator . ' ';
+        $where = implode($operator, $column_data);
+
+        if (empty($where)) {
+            return '';
+        }
+
+        $this->bound = $bound;
+
+        return 'WHERE ' . $where;
+    }
+
+    public function getBound()
+    {
+        $tmp = $this->bound;
+        $this->bound = [];
+        return $tmp;
+    }
+
+    private function logError($string = null)
+    {
+        $txt = "-------| " . date('Y-m-d H:i:s') . " |-------\n";
+        $txt .= $string . "\n";
         $txt .= '-------------------------------------' . "\n";
         $file = 'db_err.log';
         //log
         $handel = fopen($file, 'a+');
         fwrite($handel, $txt);
         fclose($handel);
+    }
+
+    private function logErrorWrapper($smt, $exception)
+    {
+        if (!$this->log_errors)
+            return;
+
+        $is_pdo_object = method_exists($smt, 'errorCode');
+        if ($is_pdo_object && $smt->errorCode() === 0000)
+            return;
+
+        $txt = "Message:\n" . $exception->getMessage() . "\n";
+        $sql = $smt->queryString;
+
+        if ($is_pdo_object) {
+            $txt .= "Error info:\n" . var_export($smt->errorInfo(), true) . "\n";
+            $txt .= "SQL:\n$sql\n";
+            $txt .= "Trace:\n" . $exception->getTraceAsString();
+        } else {
+            $txt .= "$sql";
+        }
+
+
+        $this->logError($txt);
     }
 
 }
--- test.php
+++ test.php
@@ -3,9 +3,19 @@
 include_once 'DB.php';
 
 #create object
-$DB = new DB('taskmanager', 'root', '');
+$DB = new DB('test', 'root', '', '127.0.0.1');
 
 
 //insert
-$id = $DB->insertData('tasks', ['title', 'text'], ['test', 'hello world']);
+$id = $DB->insertData('users',
+    [
+        'username',
+        'first_name',
+        'last_name',
+    ], [
+        'john_d',
+        'john',
+        'draw',
+    ]);
+
 echo 'id is: ' . $id . '<br>';
